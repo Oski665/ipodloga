bf7761741441331d1821b265ce787fb8b97d346f
Oskar Betanski <osa76autograf@o2.pl>
2023-03-28 17:17:47 +0200

    created backend + integrated with postresql database

---
+++ src/main/java/pl/edu/pbs/ipodloga/IpodlogaApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pbs.ipodloga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpodlogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpodlogaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pbs/ipodloga/IpodlogaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pbs.ipodloga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpodlogaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
