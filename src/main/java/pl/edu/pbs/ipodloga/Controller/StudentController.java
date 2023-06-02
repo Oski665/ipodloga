cdbecf0459a1b555cb82a48c14c2d190973ee3de
Oski665 <osa76autograf@o2.pl>
2023-06-03 00:02:47 +0200

    Improved controller StudentController

--- src/main/java/pl/edu/pbs/ipodloga/Controller/StudentController.java
+++ src/main/java/pl/edu/pbs/ipodloga/Controller/StudentController.java
@@ -5,7 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.pbs.ipodloga.Model.Student;
+import pl.edu.pbs.ipodloga.Model.StudentProjekt;
+import pl.edu.pbs.ipodloga.Model.StudentZadanie;
+import pl.edu.pbs.ipodloga.Service.StudentProjektService;
 import pl.edu.pbs.ipodloga.Service.StudentService;
+import pl.edu.pbs.ipodloga.Service.StudentZadanieService;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -15,11 +19,16 @@ import java.util.concurrent.ExecutionException;
 public class StudentController {
 
     private final StudentService studentService;
+    private final StudentProjektService studentProjektService;
+    private final StudentZadanieService studentZadanieService;
 
     @Autowired
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, StudentProjektService studentProjektService, StudentZadanieService studentZadanieService) {
+        this.studentProjektService = studentProjektService;
+        this.studentZadanieService = studentZadanieService;
         this.studentService = studentService;
     }
+
 
     @GetMapping
     public ResponseEntity<List<Student>> pobierzWszystkichStudentow() {
@@ -40,7 +49,10 @@ public class StudentController {
     @PostMapping("/{studentId}/projekty/{projektId}")
     public ResponseEntity<String> przypiszProjekt(@PathVariable String studentId, @PathVariable String projektId) {
         try {
-            studentService.przypiszProjekt(studentId, projektId);
+            StudentProjekt studentProjekt = new StudentProjekt();
+            studentProjekt.setStudentId(studentId);
+            studentProjekt.setProjektId(projektId);
+            studentProjektService.przypiszProjektDoStudenta(studentProjekt);
             return new ResponseEntity<>("Projekt został przypisany do studenta", HttpStatus.OK);
         } catch (InterruptedException | ExecutionException e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -52,7 +64,10 @@ public class StudentController {
     @PostMapping("/{studentId}/zadania/{zadanieId}")
     public ResponseEntity<String> przypiszZadanie(@PathVariable String studentId, @PathVariable String zadanieId) {
         try {
-            studentService.przypiszZadanie(studentId, zadanieId);
+            StudentZadanie studentZadanie = new StudentZadanie();
+            studentZadanie.setStudentId(studentId);
+            studentZadanie.setZadanieId(zadanieId);
+            studentZadanieService.przypiszZadanieDoStudenta(studentZadanie);
             return new ResponseEntity<>("Zadanie zostało przypisane do studenta", HttpStatus.OK);
         } catch (InterruptedException | ExecutionException e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
