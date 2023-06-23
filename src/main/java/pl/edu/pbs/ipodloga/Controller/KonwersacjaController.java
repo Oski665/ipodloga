3e41fea3eb1d8aa16b719ffc7018155641ed52e5
piomuc000 <piomuc000@o365.student.pbs.edu.pl>
2023-06-23 13:23:22 +0200

    Dodanie API /im/conv/{projekt_id} do wyświetlania wiadomości skojarzonych z projektem

--- src/main/java/pl/edu/pbs/ipodloga/Controller/KonwersacjaController.java
+++ src/main/java/pl/edu/pbs/ipodloga/Controller/KonwersacjaController.java
@@ -7,6 +7,8 @@ import pl.edu.pbs.ipodloga.Model.Konwersacja;
 import pl.edu.pbs.ipodloga.Model.Projekt;
 import pl.edu.pbs.ipodloga.Model.Wiadomosc;
 import pl.edu.pbs.ipodloga.Service.ProjectService;
+import java.util.List;
+import java.util.Comparator;
 
 @RestController
 @RequestMapping("/im")
@@ -18,18 +20,40 @@ public class KonwersacjaController {
         this.projectService = projectService;
     }
 
-    @PostMapping("/write/{project_id}")
+    @GetMapping("/conv/{id_projektu}")
+    public ResponseEntity<List<Wiadomosc>> pobierzWiadomosciZKonwersacji(@PathVariable("id_projektu") String idProjektu) {
+        try {
+            Projekt projekt = projectService.pobierzProjekt(idProjektu);
+            if (projekt != null) {
+                String konwersacjaId = projekt.getKonwersacja();
+                if (konwersacjaId != null) {
+                    List<Wiadomosc> wiadomosci = projectService.pobierzWiadomosci(konwersacjaId);
+                    wiadomosci.sort(Comparator.comparing(Wiadomosc::getDate));
+                    return new ResponseEntity<>(wiadomosci, HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                }
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/write/{id_projektu}")
     public ResponseEntity<String> dodajWiadomoscDoKonwersacji(
-            @PathVariable("project_id") String projectId,
+            @PathVariable("id_projektu") String idProjektu,
             @RequestBody Wiadomosc wiadomosc) {
         try {
-            Projekt projekt = projectService.pobierzProjekt(projectId);
+            Projekt projekt = projectService.pobierzProjekt(idProjektu);
             if (projekt != null) {
                 String konwersacjaId = projekt.getKonwersacja();
                 if (konwersacjaId == null) {
                     konwersacjaId = utworzNowaKonwersacja();
                     projekt.setKonwersacja(konwersacjaId);
-                    projectService.aktualizujProjekt(projectId, projekt);
+                    projectService.aktualizujProjekt(idProjektu, projekt);
                 }
                 String wiadomoscId = projectService.dodajWiadomosc(konwersacjaId, wiadomosc);
                 return new ResponseEntity<>(wiadomoscId, HttpStatus.CREATED);
